6c98ecf093a664d0401f660ecbaafa18f6935d4b
felipera8732 <felipe@pruvo.com>
2023-03-23 11:38:26 -0300

    Added transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,31 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+              <td>Work</td>
+              <td>23/03/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Lunch</td>
+              <td><PriceHighlight variant="outcome">- R$ 10,00</PriceHighlight></td>
+              <td>Food</td>
+              <td>20/03/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
